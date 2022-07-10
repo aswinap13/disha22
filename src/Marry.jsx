9207d140f3f03b758615d048634ae21396e95135
Aswin <aswinapxyz@cet.ac.in>
2022-07-10 13:09:04 +0530

    setup commit

---
+++ src/Marry.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css';
+import Spline from '@splinetool/react-spline';
+
+export default function Marry() {
+        return(
+          <div className='marry'>
+            <Spline scene="https://prod.spline.design/7QW9uO0NPVxwDjbj/scene.splinecode" />
+          </div>
+          )
+    }
---
+++ src/components/Navbare.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Navbare() {
+  return (
+    <div className='nav-absolute'> 
+        <div>
+            <ul className='nav-ul'>
+                <li>
+                    Home
+                </li>
+                <li>
+                    Score Board
+                </li>
+                <li>
+                    Live Result
+                </li>
+                <li>
+                    Schedule
+                </li>
+                <li>
+                    Team
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbare
